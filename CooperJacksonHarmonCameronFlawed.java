2a896996c2d217d6f312ad4bb920497196769f6b
Jackson-Cooper <coope5jr@dukes.jmu.edu>
2023-10-10 18:03:07 -0400

    comments

--- CooperJacksonHarmonCameronFlawed.java
+++ CooperJacksonHarmonCameronFlawed.java
@@ -33,8 +33,8 @@ public class CooperJacksonHarmonCameronFlawed {
         
         // Print the result
         System.out.println("Number of bits in N: " + bitLength);
-        System.out.println("p = " + p);
-        System.out.println("q = " + q);
+        System.out.println("p: " + p);
+        System.out.println("q: " + q);
         System.out.println("Private key d: " + d);
         System.out.println("Ciphertext: " + ciphertext);
         System.out.println("Plaintext: " + plaintext);
