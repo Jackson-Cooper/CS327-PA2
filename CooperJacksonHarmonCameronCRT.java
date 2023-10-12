01f261a4b08fcc450a82dc243938a97a5cf140c5
Cameron Harmon <harmoncc@dukes.jmu.edu>
2023-10-12 10:41:31 -0400

    crt

---
+++ CooperJacksonHarmonCameronCRT.java
@@ -0,0 +1,117 @@
+import java.math.BigInteger;
+
+public class CooperJacksonHarmonCameronCRT {
+    public static BigInteger[] encryptWithPrint() {
+        BigInteger[] arr = new BigInteger[5];
+
+        // Define e as a BigInteger
+        BigInteger e = new BigInteger("65537");
+
+        String hexP = "b6fa23706cb1881090a895dd8c0321ba52004073" +
+        "39e487fa3d554541ff9590277c1343f53e30da7fa7a968f7989a60e" +
+        "7ad996a22ae443109a0421ece3b9c29cda22edeab53a79dfef83b10" +
+        "1ad6f06763860cfd8f3762c96d302b5d289458960223ea5ae1eddd7" +
+        "1d9cf4f70f69384dbc1ee023cfebcc156b8fe5506f3c2e2498f4820" +
+        "a2867ff4530a0b1af6f93fbc4f28099ed840b80b2efce3046827bd9" +
+        "fd8bebbc20c7932540ef7a982a533d966bf9a399fb2f96e6acfc0ca94a9be5457d2a5";
+
+        String hexQ = "d0bf42f8ad2cd77b17a06887497f9708bad7d4cb" +
+        "a6e2d882c112913b7194d6a41ed5942819da31c23e42491e2367188" +
+        "f60228ccebf566d32bc4214461e7b65ac6e956f48f00c8d233ef90e" +
+        "982bbf8d5fc55f388fe33f6bd3db0eea97c4613e984348ae88592eb" +
+        "70b64ecc0b33835eed89e6dc0dc8f74eb35e8db6f7e29ef27ec6d06" +
+        "cc4d99f702c864b45004c105494312ffa7ecc3a902241e6d88d7912" +
+        "8b6b1dd8a6b1f36a329603eca663d97269f4266d85bc0a637f648f8334cb30c224f0d";
+
+        BigInteger p = new BigInteger(hexP, 16);
+        BigInteger q = new BigInteger(hexQ, 16);
+        arr[0] = p;
+        arr[1] = q;
+        
+        BigInteger N = p.multiply(q);
+        arr[2] = N;
+
+        BigInteger m = new BigInteger("3");
+        
+        // Calculate the number of bits in N
+        int bitLength = N.bitLength();
+
+        // Calculate (p-1) and (q-1)
+        BigInteger pMinus1 = p.subtract(BigInteger.ONE);
+        BigInteger qMinus1 = q.subtract(BigInteger.ONE);
+        
+        // Calculate the modular inverse of e modulo ((p-1)(q-1))
+        BigInteger d = e.modInverse(pMinus1.multiply(qMinus1));
+        arr[3] = d;
+
+        // Calculate ciphertext
+        BigInteger ciphertext = m.modPow(e, N);
+        arr[4] = ciphertext;
+
+        // CRT decryption process
+        BigInteger dp = d.mod((p.subtract(BigInteger.ONE)));
+        BigInteger dq = d.mod((q.subtract(BigInteger.ONE)));
+        
+        BigInteger mp = ciphertext.modPow(dp, p);
+        BigInteger mq = ciphertext.modPow(dq, q);
+
+        BigInteger a = mp.multiply(q).multiply(q.modInverse(p)).add(mq.multiply(p).multiply(p.modInverse(q)));
+
+        // decrypt ciphertext
+        BigInteger plaintext = a.mod(N);
+        
+        // Print the result
+        System.out.println("p = " + p.toString(16));
+        System.out.println("q = " + q.toString(16));
+        System.out.println("N = " + N.toString(16));
+        System.out.println("Bit-length of N = " + Integer.toString(bitLength, 16));
+        System.out.println("e = " + e.toString(16));
+        System.out.println("d = " + d.toString(16));
+        System.out.println("c = " + ciphertext.toString(16));
+        System.out.println("m2 = " + plaintext.toString(16));
+
+        return arr;
+    }
+
+    public static void decryption(BigInteger p, BigInteger q, BigInteger N, BigInteger d, BigInteger ciphertext) {
+        // CRT decryption process
+        BigInteger dp = d.mod((p.subtract(BigInteger.ONE)));
+        BigInteger dq = d.mod((q.subtract(BigInteger.ONE)));
+        
+        BigInteger mp = ciphertext.modPow(dp, p);
+        BigInteger mq = ciphertext.modPow(dq, q);
+
+        BigInteger a = mp.multiply(q).multiply(q.modInverse(p)).add(mq.multiply(p).multiply(p.modInverse(q)));
+
+        // decrypt ciphertext
+        a.mod(N);
+    }
+    
+    public static void main(String[] args) {
+        BigInteger[] dN = encryptWithPrint();
+        
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            decryption(dN[0], dN[1], dN[2], dN[3], dN[4]);
+        }
+
+        long endTime = System.currentTimeMillis();
+        float elapsedTime = (float) (endTime - startTime);
+        
+        BigInteger N = dN[2];
+
+        int bitLength = N.bitLength();
+
+        float kbps = (float) (bitLength / (elapsedTime / 1000.0) / 1000.0);
+        float gbps = (float) (kbps / 1000.0);
+
+        System.out.printf("CRT Decryptions took %.0f milliseconds", elapsedTime);
+        System.out.println();
+        System.out.printf("CRT Decryptions took %.3f kilobits/second ", kbps);
+        System.out.println();
+        System.out.printf("This speed is %e gigabit/second Internet speed", gbps);
+        System.out.println();
+
+    }
+}
